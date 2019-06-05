faa6a885ed43118ed11bba714c860a25f01c2996
echeva <yaniiecheva@gmail.com>
2019-06-04 21:44:31 -0300

    Concurrente 2019

---
+++ Concurrente/Resoluciones/Practica5/ejercicio5.java
@@ -0,0 +1,14 @@
+/*5) En una clínica existe un médico de guardia que recibe continuamente peticiones de
+atención de las E enfermeras que trabajan en su piso y de las P personas que llegan a la
+clínica ser atendidos.
+Cuando una persona necesita que la atiendan espera a lo sumo 5 minutos a que el
+médico lo haga, si pasado ese tiempo no lo hace, espera 10 minutos y vuelve a requerir
+la atención del médico. Si no es atendida tres veces, se enoja y se retira de la clínica.
+Cuando una enfermera requiere la atención del médico, si este no lo atiende
+inmediatamente le hace una nota y se la deja en el consultorio para que esta resuelva su
+pedido en el momento que pueda (el pedido puede ser que el médico le firme algún papel). 
+Cuando la petición ha sido recibida por el médico o la nota ha sido dejada en el
+escritorio, continúa trabajando y haciendo más peticiones.
+El médico atiende los pedidos dándoles prioridad a los pacientes que llegan para ser
+atendidos. Cuando atiende un pedido, recibe la solicitud y la procesa durante un cierto
+tiempo. Cuando está libre aprovecha a procesar las notas dejadas por las enfermeras.*/
