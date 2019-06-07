bb115f5b7928ba6bc5e904725cff2674a929dc3e
echeva <yaniiecheva@gmail.com>
2019-06-07 14:46:50 -0300

    Resolucion de parciales

--- Concurrente/Resoluciones/Practica4/notas.java
+++ Concurrente/Resoluciones/Practica4/notas.java
@@ -8,3 +8,13 @@ si pueden esperar eternamente a que haya un mensaje*/
 /*PMS tanto el send como el receive son bloqueantes.*/
 //el do recorre cuantas veces sea necesario, hasta que todas las sentencias sean falsas
 
+ Radar[*] //Recibo un mensaje de cualquier proceso RADAR
+ //Como el send es bloqeuante, se necesita un proceso intermedio que reciba antes
+ //de que su muestra pueda ser procesada por la unidad
+
+/*PMS Hasta que el otro proceso no reciba un msj, no puedo continuar.
+ tanto el send como el receive son bloqueantes y no tengo cola de mensajes
+Radares: 
+ cpu! -> el send es con !
+ Radar[*] -> esto quiere decir que recibo msj de cualquier proceso Radar
+ Si ponemos un process admin que encola todas las señales que recibe de radar, */
