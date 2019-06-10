b2a5b34085777d1fb76a9e72f47b9d6a3714c430
echeva <yaniiecheva@gmail.com>
2019-06-10 11:28:48 -0300

    Parcial 1 pms

---
+++ Concurrente/Resoluciones/Parciales_Parte2/pms_parcial3.java
@@ -0,0 +1,6 @@
+/*Resolver con PMS el siguiente problema: se debe administrar el acceso para usar
+un determinado Servidor donde no se permiten más de 10 usuarios trabajando al mismo
+tiempo, por cuestiones de rendimiento. Existen N usuarios que solicitan acceder al 
+Servidor, esperan hasta que se les de acceso para trabajar en él y luego salen del
+mismo. Nota: suponga que existe una función TrabajarEnServidor() que llaman los 
+usuarios para representar que están trabajando en el Servidor.*/
